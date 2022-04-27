fa79c810baee28912abad5a73d3c6f555f371a4b
CBBell99 <chrisbeckertonbell@gmail.com>
2022-04-26 22:17:31 -0700

    create EditListing page

---
+++ src/pages/EditListing.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function EditListing() {
+  return <div>EditListing</div>;
+}
+
+export default EditListing;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -98,6 +98,8 @@ function Profile() {
     }
   };
 
+  const onEdit = (listingId) => navigate(`/edit-listing/${listingId}`);
+
   return (
     <div className="profile">
       <header className="profileHeader">
@@ -154,6 +156,7 @@ function Profile() {
                   listing={listing.data}
                   id={listing.id}
                   onDelete={() => onDelete(listing.id)}
+                  onEdit={() => onEdit(listing.id)}
                 />
               ))}
             </ul>
